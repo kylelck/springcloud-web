0132ce037edf571964c33ef2f5e8cedbfd4ecae4
lichengkai <lick@hsyuntai.com>
2017-12-07 17:36:14 +0800

    hystrix

--- src/main/java/com/lick/SpringcloudWebApplication.java
+++ src/main/java/com/lick/SpringcloudWebApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrixDashboard
 /**@SpringCloudApplication 注解包含三个注解： @SpringBootApplication、@EnableDiscoveryClient、@EnableCircuitBreaker*/
 @SpringCloudApplication
 public class SpringcloudWebApplication {
